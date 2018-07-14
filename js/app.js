5d09da783d7e094c697cc9ae4a0893a309c4eaef
Thuan Anh Bui <lucasbui92@gmail.com>
2018-07-14 23:35:36 +1000

    Modified the files

--- js/app.js
+++ js/app.js
@@ -9,6 +9,11 @@ var cardList = document.querySelectorAll('.card');
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+var listOfCards = [];
+for (var i = 0; i < cardList.length; i++) {
+  listOfCards.push(cardList[i]);
+}
+cardList = shuffle(listOfCards);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -39,16 +44,31 @@ function shuffle(array) {
 var openCards = [];
 var count = 0;
 var click = 0;
+var timeStart = 0;
+var time = 0;
 var counter = document.querySelector('.moves');
 const mainHeading = document.querySelector('header');
 const restartGame = document.querySelector('.restart');
-var winText;
 
 //Increment the counter for each click
 function increase_counter() {
   click += 1;
-  counter.innerHTML = click
+  counter.innerHTML = click;
   return counter.innerHTML;
+}
+
+//Remove stars based on performance
+function remove_star() {
+  const starList = document.querySelector('.stars');
+  const a = starList.querySelector('li');
+  const stars = starList.querySelector('li > i');
+
+  if (counter.innerHTML == 18) {
+    a.remove(stars);
+  }
+  else if (counter.innerHTML == 26) {
+    a.remove(stars);
+  }
 }
 
 //Reveal the cards
@@ -56,7 +76,6 @@ function reveal(card) {
   if (!card.classList.contains('open') || !card.classList.contains('show')) {
     card.classList.add('open', 'show');
     openCards.push(card);
-    console.log(card);
   }
 }
 
@@ -69,28 +88,62 @@ function checking(card) {
         count += 1;
       });
     }
-    openCards.forEach(function(card) {
-      card.classList.remove('open', 'show');
-    })
-    openCards = [];
+    setTimeout(function() {
+      openCards.forEach(function(card) {
+        card.classList.remove('open', 'show');
+      });
+      openCards = [];
+    }, 1000);
   }
 }
 
 //Check for the winning condition
 function winning(value) {
-  winText = "<p>You Win!!! Your score is </p>";
+  var winText = "<p>You Win!!! Your score is " + value + "</p>";
   if (count == cardList.length) {
+    /*const modal = document.querySelector('.modal');
+    const modalContent = document.createElement('p');
+    modalContent.textContent = "You Win";
+    modal.style.cssText = 'text-align: center; justify-content: center; align-items: center';
+    modal.appendChild(modalContent);*/
     mainHeading.insertAdjacentHTML('afterend', winText);
+    const a = document.querySelector('.container > p');
+    a.classList.add('winText');
   }
+}
+
+//Run the timer
+function count_time() {
+  setTimeout(function() {
+    time += 1;
+    var mins = Math.floor(time/10/60);
+    var secs = Math.floor(time/10%60);
+    var hours = Math.floor(time/10/60/60);
+    if (mins < 10) {
+      mins = '0' + mins;
+    }
+    if (secs < 10) {
+      secs = '0' + secs;
+    }
+    document.querySelector('.timer').innerHTML = hours + ":" + mins + ":" + secs;
+    if (count != cardList.length) {
+      count_time();
+    }
+  }, 100);
 }
 
 //Set up action events for each card
 cardList.forEach(function(card) {
   card.addEventListener('click', function(c) {
     var value = increase_counter();
+    remove_star();
     reveal(card);
     checking(card);
     winning(value);
+    timeStart += 1;
+    if (timeStart == 1) {
+      count_time();
+    }
   });
 });
 
@@ -106,8 +159,50 @@ restartGame.addEventListener('click', function() {
   click = 0;
   counter.innerHTML = 0;
 
+  //Add 3 stars
+  const starList = document.querySelector('.stars');
+  const stars = starList.querySelectorAll('li');
+  if (stars.length == 1) {
+    for (var i = 0; i < 2; i++) {
+      const starList = document.querySelector('.stars');
+      const newList = document.createElement('li');
+      const newStar = document.createElement('i');
+      starList.appendChild(newList);
+      newList.appendChild(newStar);
+    }
+
+    const c = starList.querySelectorAll('li > i');
+    for (var j = 0; j < c.length; j++) {
+      if (c[j].classList.contains('fa') == false && c[j].classList.contains('fa-star') == false) {
+        c[j].classList.add('fa');
+        c[j].classList.add('fa-star');
+      }
+    }
+  }
+  else if (stars.length == 2) {
+    const starList = document.querySelector('.stars');
+    const newList = document.createElement('li');
+    const newStar = document.createElement('i');
+    starList.appendChild(newList);
+    newList.appendChild(newStar);
+
+    const c = starList.querySelectorAll('li > i');
+    for (var j = 0; j < c.length; j++) {
+      if (c[j].classList.contains('fa') == false && c[j].classList.contains('fa-star') == false) {
+        c[j].classList.add('fa');
+        c[j].classList.add('fa-star');
+      }
+    }
+  }
+
+  //Reset the timer
+  time = 0;
+  document.querySelector('.timer').innerHTML = "0:00:00";
+
   //Remove the winning message
   var para = document.querySelector('.container');
-  var message = para.querySelector('p');
-  message.remove();
+  var message = para.querySelector('.winText');
+  if (message.classList.contains('winText') == true) {
+      message.remove();
+  }
 });
